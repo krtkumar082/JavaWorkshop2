371cb8b24a0d914cf11c4f6063c615dd33226202
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-10-18 21:11:04 +0530

    [Kirti]:Added:ability to validate hotel name and to find cheapest best rated hotel and best rated hotel using streams

--- src/main/java/com/workshop2/HotelReservation.java
+++ src/main/java/com/workshop2/HotelReservation.java
@@ -14,15 +14,15 @@ public class HotelReservation {
 	
 	public static void cheapestHotelfunc() throws InvalidCustomerException {
 		try {
-	 		System.out.println("enter the start date-(YYYY-MM-DD)");
+	 		System.out.println("Enter the Start Date-(YYYY-MM-DD)");
 	 		LocalDate startDate=LocalDate.parse(sc.next());
-	 		System.out.println("enter the end date-(YYYY-MM-DD)");
+	 		System.out.println("Enter the End Date-(YYYY-MM-DD)");
 	 		LocalDate endDate=LocalDate.parse(sc.next());
 	 		String custType=null;
 	 	      
 	 		do {
 	 			try {
-	 	    	  System.out.println("enter the customer type - Reward/Regular");
+	 	    	  System.out.println("Enter the Customer Type - Reward/Regular");
 	 	    	  custType=sc.next();
 	 	    	  if(custType.equalsIgnoreCase("Reward") || custType.equalsIgnoreCase("Regular"))
 	 	    		  break;
@@ -36,21 +36,21 @@ public class HotelReservation {
 	 	int cost=0;	
 	 	cost=f1.getCheapestHotel(startDate,endDate,custType);
  		}catch(DateTimeException e) {
- 			System.out.println("Invalid date format");
+ 			System.out.println("Invalid Date Format");
  		} 
 	}
 	
 	public static void bestRatedHotel() throws InvalidCustomerException{
 		try {
-	 		System.out.println("enter the start date-(YYYY-MM-DD)");
+	 		System.out.println("Enter the Start Date-(YYYY-MM-DD)");
 	 		LocalDate startDate=LocalDate.parse(sc.next());
-	 		System.out.println("enter the end date-(YYYY-MM-DD)");
+	 		System.out.println("Enter the End Date-(YYYY-MM-DD)");
 	 		LocalDate endDate=LocalDate.parse(sc.next());
 	 		String custType=null;
 	 		
 	 		do {
 	 			try {
-	 	    	  System.out.println("enter the customer type - Reward/Regular");
+	 	    	  System.out.println("Enter the Customer Type - Reward/Regular");
 	 	    	  custType=sc.next();
 	 	    	  if(custType.equalsIgnoreCase("Reward") || custType.equalsIgnoreCase("Regular"))
 	 	    		  break;
@@ -64,42 +64,42 @@ public class HotelReservation {
 	 	int cost=0;	
 	 	cost=f1.bestRatedHotel(startDate,endDate,custType);
  		}catch(DateTimeException e) {
- 			System.out.println("Invalid date format");
+ 			System.out.println("Invalid Date Format");
  		} 
 	}
 	
 	public static void main(String[] args) throws InvalidCustomerException {
-	System.out.println(" Welcome to hotel management system");
+	System.out.println(" Welcome to Hotel Management System");
 	
 	 int option=0;
 	 do{
-		 System.out.println("Enter the option to choose\n"+"1)add hotel\n"+"2) cheapest hotel\n"
-	                        +"3) Best Rated Hotel\n"
+		 System.out.println("Enter the option to choose\n"+"1)add hotel\n"+"2)cheapest hotel\n"
+	                        +"3)Best Rated Hotel\n"
 	                        +"4)exit");
 		 option=sc.nextInt();
 		 switch(option) {
 		 case 1:do {
-			 		System.out.println("enter hotel name");
+			 		System.out.println("Enter Hotel Name");
 			 		String name=sc.next();
 			 		boolean isValid=valid.validate(name);
 			 		while(!isValid) {
-			 			System.out.println("enter hotel name again in proper valid format");
+			 			System.out.println("Enter Hotel Name again in proper valid format");
 			 			name=sc.next();
 			 			isValid=valid.validate(name);
 			 		}
-			 		System.out.println("enter the rate of the hotel for regular customer for week days");
+			 		System.out.println("Enter the rate of the hotel for regular customer for week days");
 			 		int rateWeekDayRegCust=sc.nextInt();
-			 		System.out.println("enter the rate of the hotel for regular customer for weekend days");
+			 		System.out.println("Enter the rate of the hotel for regular customer for weekend days");
 			 		int rateWeekNdayRegCust=sc.nextInt();
-			 		System.out.println("enter the rate of the hotel for reward customer for week days");
+			 		System.out.println("Enter the rate of the hotel for reward customer for week days");
 			 		int rateWeekDayRewdCust=sc.nextInt();
-			 		System.out.println("enter the rate of the hotel for reward customer for weekend days");
+			 		System.out.println("Enter the rate of the hotel for reward customer for weekend days");
 			 		int rateWeekNdayRewdCust=sc.nextInt();
 			 		System.out.println("Enter the hotel rating");
 			 		int rating=sc.nextInt();
 			 		f1.addHotel(name,rateWeekDayRegCust,rateWeekNdayRegCust
 			 				    ,rateWeekDayRewdCust,rateWeekNdayRewdCust,rating);
-			 		System.out.println("want to add more hotel Y/N");
+			 		System.out.println("Wants to add more Hotel Y/N");
 					 char ch=sc.next().charAt(0);
 					 if(ch=='Y')
 						 continue;
@@ -118,7 +118,7 @@ public class HotelReservation {
 	 			break;
 	 			
 	 }	 
-		 System.out.println("want to continue Y/N");
+		 System.out.println("Wants to continue Y/N");
 		 char ch=sc.next().charAt(0);
 		 if(ch=='Y')
 			 continue;
